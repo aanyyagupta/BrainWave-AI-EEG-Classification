5f793951cb3589f459eedd243f9e5b88aec17eea
aanyyagupta <aanya.gupta0620@gmail.com>
2026-07-07 22:27:33 +0530

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+src package
+------------
+WHY THIS FILE EXISTS:
+    This empty (well, documented) file tells Python "the src folder is a
+    package," which means other files can do:
+        from src.preprocessing import handle_missing_values
+    instead of messy relative imports. It's a Python convention -
+    every folder you want to import from needs an __init__.py file.
+"""
